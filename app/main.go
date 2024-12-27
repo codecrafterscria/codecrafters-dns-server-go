90a7a3b08d3c9f9bedc5688fc00bf84f0b28ecee
Pedro Tashima <tithe.coup0l@icloud.com>
2024-12-27 12:11:43 -0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 )
+
+type DNSHeader struct {
+	ID      uint
+	QR      uint
+	Opcode  uint
+	AA      uint
+	TC      uint
+	RD      uint
+	RA      uint
+	Z       uint
+	RCODE   uint
+	QDCOUNT uint
+	ANCOUNT uint
+	NSCOUNT uint
+	ARCOUNT uint
+}
+
+func NewDNSHeader() *DNSHeader {
+	return &DNSHeader{}
+}
+
+func (h *DNSHeader) Parse(data []byte) {
+	fmt.Printf("Data: %08b\n", data)
+}
 
 func main() {
 	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
@@ -31,8 +56,13 @@ func main() {
 		receivedData := string(buf[:size])
 		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
 
+		h := NewDNSHeader()
+		h.Parse(buf[:12])
+
 		// Create an empty response
-		response := []byte{}
+		response := make([]byte, 12)
+		binary.BigEndian.PutUint16(response[0:2], 1234)
+		response[2] = 1 << 7
 
 		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
